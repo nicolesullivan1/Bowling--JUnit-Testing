48b29c4bc6cc6daca12a2c0bd7d45e2eaeacb652
Nicole <nicole.sullivan1@bison.howard.edu>
2020-04-17 19:26:16 +0000

    Adding comments to tests

--- main/MyTests.java
+++ main/MyTests.java
@@ -7,42 +7,49 @@ public class MyTests {
 //Testing return method CalculateRolls
   @Test
   public void RollsAllStrikes() {
+    //Test to calculate total rolls for rolling all Strikes
 		CalculateScore test = new CalculateScore ("X-X-X-X-X-X-X-X-X-XXX");
 		assertEquals(12, test.CalculateRolls("X-X-X-X-X-X-X-X-X-XXX"));
   }
 
   @Test
   public void RollsAllSpares() {
+    //Test to calculate total rolls for rolling all Spares
 		CalculateScore test = new CalculateScore ("5/-5/-5/-5/-5/-5/-5/-5/-5/-5/0");
 		assertEquals(21, test.CalculateRolls("5/-5/-5/-5/-5/-5/-5/-5/-5/-5/0"));
   }
 
   @Test
   public void RollsAllOpenFrames() {
+    //Test to calculate total rolls for rolling all Open Frames
 		CalculateScore test = new CalculateScore ("12-12-12-12-12-12-12-12-12-12");
 		assertEquals(20, test.CalculateRolls("12-12-12-12-12-12-12-12-12-12"));
   }
 
   @Test
   public void RollsXSparesOFs() {
+    //Test to calculate total rolls for rolling strikes, spares, and open frames
 		CalculateScore test = new CalculateScore ("12-X-9/-12-X-9/-23-5/-20-12");
 		assertEquals(18, test.CalculateRolls("12-X-9/-12-X-9/-23-5/-20-12"));
   }
 
   @Test
   public void RollsXSpares() {
+    //Test to calculate total rolls for rolling strikes and spares
 		CalculateScore test = new CalculateScore ("X-X-9/-X-X-9/-X-5/-X-XXX");
 		assertEquals(15, test.CalculateRolls("X-X-9/-X-X-9/-X-5/-X-XXX"));
   }
 
   @Test
   public void RollsSparesOFs() {
+    //Test to calculate total rolls for rolling spares and open frames
 		CalculateScore test = new CalculateScore ("9/-23-9/-5/-72-22-7/-12-44-50");
 		assertEquals(20, test.CalculateRolls("9/-23-9/-5/-72-22-7/-12-44-50"));
   }
 
   @Test
   public void StrikesOFs() {
+    //Test to calculate total rolls for rolling strikes and open frames
 		CalculateScore test = new CalculateScore ("X-23-X-X-72-22-X-12-44-50");
 		assertEquals(16, test.CalculateRolls("X-23-X-X-72-22-X-12-44-50"));
   }
@@ -50,60 +57,70 @@ public class MyTests {
 //Testing overall Calculations
   @Test
   public void PerfectGame() {
+    //Testing score for perfect game 
 		CalculateScore test = new CalculateScore ("X-X-X-X-X-X-X-X-X-XXX");
 		assertEquals(300, test.GetTotal());
   }
 
   @Test
   public void AddUpToNine() {
+    //Testing score for frames that add up to 9
 		CalculateScore test = new CalculateScore ("45-54-36-27-09-63-81-18-90-72");
 		assertEquals(90, test.GetTotal());
   }
 
   @Test
   public void SparesOf5() {
+    //Testing score for frames that add up to spares
 		CalculateScore test = new CalculateScore ("5/-5/-5/-5/-5/-5/-5/-5/-5/-5/0");
 		assertEquals(145, test.GetTotal());
   }
 
   @Test
   public void GameOfZero() {
+    //Testing score for game of zero
 		CalculateScore test = new CalculateScore ("00-00-00-00-00-00-00-00-00-00");
 		assertEquals(0, test.GetTotal());
   }
 
   @Test
   public void TenthFrameOF() {
+    //Testing score for all strikes and one OF
 		CalculateScore test = new CalculateScore ("X-X-X-X-X-X-X-X-X-10");
 		assertEquals(243, test.GetTotal());
   }
 
   @Test
   public void TenthFrameSpare() {
+    //Testing score for all strikes an one spare
 		CalculateScore test = new CalculateScore ("X-X-X-X-X-X-X-X-X-7/3");
 		assertEquals(270, test.GetTotal());
   }
 
   @Test
   public void TenthFrameStrike() {
+    //Testing score for all strikes then one OF
 		CalculateScore test = new CalculateScore ("X-X-X-X-X-X-X-X-X-X01");
 		assertEquals(271, test.GetTotal());
   }
 
   @Test
   public void RollsSparesAndX() {
+    //Testing score for all spares and one strike
 		CalculateScore test = new CalculateScore ("X-9/-X-9/-X-9/-X-9/-X-9/1");
 		assertEquals(191, test.GetTotal());
   }
 
   @Test
   public void RollsVarious() {
+    //Testing score for various frames
 		CalculateScore test = new CalculateScore ("X-9/-X-11-X-9/-X-11-X-9/1");
 		assertEquals(139, test.GetTotal());
   }
 
   @Test
   public void StrikesAndOFs() {
+    //Testing score for mixture of strikes and OFs
 		CalculateScore test = new CalculateScore ("X-90-X-90-X-90-X-90-X-90");
 		assertEquals(140, test.GetTotal());
   }
