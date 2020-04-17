6fb32c45a81b5e7f90b22ca385c426d54b679df5
Nicole <nicole.sullivan1@bison.howard.edu>
2020-04-17 19:31:32 +0000

    Adding Javadocs and regular comments

--- main/TestRunner.java
+++ main/TestRunner.java
@@ -4,14 +4,22 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+/**
+ * @author nicolesullivan
+ * TestRunner class to run JUnit Tests
+ * @since   2020-04-02 
+ */
 public class TestRunner {
   public static void main() {
     Result result = JUnitCore.runClasses(MyTests.class);
     
+    //Print out failures
     for (Failure failure : result.getFailures()) {
       System.out.println(failure.toString());
     }
 
+    //Prints out total test cases, the # of sucessfull cases,
+    //and the # of failed cases
     System.out.println("TEST RESULTS");
     System.out.println("Total Test Cases: " + result.getRunCount());
     System.out.print("Successful: " + (result.getRunCount() - result.getFailureCount()));  
